236c03ed3964069c651efef65f1fc74e60d26ba2
Divine Ezelibe <60388882+divineezelibe@users.noreply.github.com>
2023-01-24 15:06:47 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,7 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
-//#include <stdarg.h>
+#include <stdarg.h>
 
+int _write(char c);
 int _printf(const char *format, ...);
+int _print_a_char(va_list args);
+int _print_a_string(va_list args);
+int _print_format(const char *format, va_list args);
+int _print_spec(char format, va_list args);
+int _print_invalid_spec(char prev_format, char format, int count);
+int _print_a_integer(va_list args);
+void _recursion_integer(int a);
+int _print_int_binary(va_list args);
+void _recursion_int_binary(int a);
+int _validate_char(char _type);
 
 #endif
