9b74bfc9fd46d6ca9208a6b254fa4f28c2628847
Debbylight <117994205+Debbylight@users.noreply.github.com>
2023-01-24 18:18:15 +0100

    Update 0-functions.c

--- 0-functions.c
+++ 0-functions.c
@@ -1,7 +1,5 @@
 #include "main.h"
-
 /************************* PRINT CHAR *************************/
-
 /**
  * print_char - Prints a char
  * @types: List a of arguments
@@ -13,11 +11,10 @@
  * Return: Number of chars printed
  */
 int print_char(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+        int flags, int width, int precision, int size)
 {
-	char c = va_arg(types, int);
-
-	return (handle_write_char(c, buffer, flags, width, precision, size));
+        char c = va_arg(types, int);
+        return (handle_write_char(c, buffer, flags, width, precision, size));
 }
 /************************* PRINT A STRING *************************/
 /**
