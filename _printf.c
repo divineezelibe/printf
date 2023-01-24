05e22dd3bcb781fc739f8aa0896f597a4db5c4be
Divine Ezelibe <60388882+divineezelibe@users.noreply.github.com>
2023-01-24 14:46:12 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -6,24 +6,30 @@
 /**
  * _printf - Printf function
  * @format: format.
+ * @args: ...
  * Return: Printed chars.
  */
 
 int _printf(const char *format, ...)
 {
+        va_list args;
         int length;
         int i=0;
 
         if (format == NULL)
                 return (-1);
 
+        va_start(args, format);
+
         while (format && format[i])
         {
-                if (format[i] == '\0')
+                if (format[i] == NULL)
                         return (-1);
                 length+=write(1,&format[i],1);
                 i++;
         }
 
-        return length;
+        va_end(args);
+
+        return (length);
 }
